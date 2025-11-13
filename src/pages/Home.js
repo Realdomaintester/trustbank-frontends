cd0737710a93cba072f1ed70ff4a354576ec1b1f
Realdomaintester <fedexagentss3457@gmail.com>
2025-11-13 02:59:06 +0100

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Welcome to TrustBank</h2>
+      <p>This is the home page of your frontend app.</p>
+    </div>
+  );
+}
